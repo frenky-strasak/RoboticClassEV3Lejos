90ccf2ecee8a6a91e197fd72ce090ef50ac6507a
Frenky Střasák <frenky.picasso@seznam.cz>
2020-03-10 16:21:14 +0100

    Create Main.java

---
+++ pack/Main.java
@@ -0,0 +1,17 @@
+package pack;
+
+import lejos.hardware.Button;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DriveRobot d = new DriveRobot();
+		d.go();
+//		System.out.println("Ahoj mami");
+//        Button.waitForAnyPress();
+	}
+
+}
